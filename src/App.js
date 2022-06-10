e219f28d2f5c4389d2e4655fa51249ca9e6dfc81
PRØ GAMƏR <galbadrakhtergel0820@gmail.com>
2022-06-11 00:24:34 +0800

    2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { FXAAShader } from "three/examples/jsm/shaders/FXAAShader";
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
 import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
 import { Canvas, useFrame, extend, useThree } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, CameraShake, Stats } from "@react-three/drei";
 import robotoRegular from "./styles/Roboto_Regular.json";
 import "./styles/App.scss";
 
@@ -20,8 +20,42 @@ extend({
   TextGeometry,
 });
 
-const Box = (props) => {
+// const Box = (props) => {
+//   const ref = useRef();
+//   // useFrame((state, delta) => (ref.current.rotation.x += 0.005, ref.current.rotation.y += 0.005));
+//   const [hovered, setHover] = useState(false);
+//   const [active, setActive] = useState(false);
+//   return (
+//     <mesh
+//       ref={ref}
+//       {...props}
+//       onClick={(event) => setActive(!active)}
+//       onPointerOver={(event) => setHover(true)}
+//       onPointerOut={(event) => setHover(false)}
+//     >
+//       <boxGeometry attach="geometry" args={[1, 1, 1]} />
+//       <meshStandardMaterial
+//         attach="material"
+//         color={hovered ? "#fff" : "#202020"}
+//       />
+//     </mesh>
+//   );
+// };
+
+const CircleGeometry = (props) => {
   const ref = useRef();
+  // useFrame(() => ref.current && void (ref.current.rotation.y += 0.01));
+  return (
+    <mesh ref={ref} {...props}>
+      <circleGeometry attach="geometry" args={[1.05, 128]} />
+      <meshBasicMaterial attach="material" color="#371B58" side={DoubleSide} />
+    </mesh>
+  );
+};
+
+const CylinderGeometry = (props) => {
+  const ref = useRef();
+  useFrame(() => (ref.current.rotation.x = 1.57));
   // useFrame((state, delta) => (ref.current.rotation.x += 0.005, ref.current.rotation.y += 0.005));
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
@@ -33,22 +67,18 @@ const Box = (props) => {
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}
     >
-      <boxGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshStandardMaterial
-        attach="material"
-        color={hovered ? "#fff" : "#202020"}
-      />
+      <cylinderBufferGeometry attach="geometry" args={[0.9, 0.9, 0.2, 64]} />
+      <meshStandardMaterial attach="material" color="#371B58" />
     </mesh>
   );
 };
 
-const CircleGeometry = (props) => {
+const SphereGeometry = (props) => {
   const ref = useRef();
-  // useFrame(() => ref.current && void (ref.current.rotation.y += 0.01));
   return (
     <mesh ref={ref} {...props}>
-      <circleGeometry attach="geometry" args={[1, 128]} />
-      <meshBasicMaterial attach="material" color="pink" side={DoubleSide} />
+      <sphereBufferGeometry attach="geometry" args={[0.5, 64, 64]} />
+      <meshStandardMaterial attach="material" color="#7858A6" />
     </mesh>
   );
 };
@@ -80,9 +110,27 @@ const Text = (props) => {
   const font = new FontLoader().parse(robotoRegular);
   return (
     <mesh {...props}>
-      <textGeometry args={["OB2O", { font, size: 0.5, height: 0.1 }]} />
-      <meshPhysicalMaterial attach="material" color={"cyan"} />
+      <textGeometry args={["OB2O", { font, size: 0.5, height: 0.3 }]} />
+      <meshPhysicalMaterial attach="material" color="#7858A6" />
     </mesh>
+  );
+};
+
+const Obso = (props) => {
+  const ref = useRef();
+  useFrame(
+    () =>
+      ref.current &&
+      void ((ref.current.position.y += 0), (ref.current.rotation.y += 0.05))
+  );
+  return (
+    <group ref={ref}>
+      <mesh {...props}>
+        <CircleGeometry position={[0, 0, 0]} />
+        <CylinderGeometry />
+        <Text position={[-0.9, -0.2, -0.15]} />
+      </mesh>
+    </group>
   );
 };
 
@@ -103,10 +151,18 @@ const App = () => {
         onCreated={(state) => state.gl.setClearColor("#0f0f15")}
       >
         <Effect />
-        <OrbitControls />
+        {/* <OrbitControls /> */}
+        {/* <CameraShake
+          maxYaw={0.01}
+          maxPitch={0.01}
+          maxRoll={0.01}
+          yawFrequency={0.5}
+          pitchFrequency={0.5}
+          rollFrequency={0.4}
+        /> */}
         <ambientLight intensity={1} />
         <spotLight position={[10, 15, 10]} angle={0.5} />
-        <primitive object={new AxesHelper(100)} />
+        {/* <primitive object={new AxesHelper(100)} /> */}
         {/* <Box position={[0, 0, 0]} />
         <Box position={[0, 0, 1.1]} />
         <Box position={[0, 0, -1.1]} />
@@ -134,8 +190,12 @@ const App = () => {
         <Box position={[-1.1, -1.1, 0]} />
         <Box position={[-1.1, -1.1, 1.1]} />
         <Box position={[-1.1, -1.1, -1.1]} /> */}
-        <CircleGeometry position={[0, 0, 0]} />
-        <Text position={[1, 0, 0]} />
+        {/* <CircleGeometry position={[0, 0, 0]} />
+        <CylinderGeometry />
+        <Text position={[-0.9, -0.2, -0.15]} /> */}
+        <Obso />
+        <SphereGeometry position={[0, 2, 0]} />
+        <Stats />
       </Canvas>
       <div className="word-con">
         <h1 className="word word-1">OB2O</h1>
@@ -145,3 +205,5 @@ const App = () => {
 };
 
 export default App;
+
+//https://colorhunt.co/palette/371b584c35755b4b8a7858a6
